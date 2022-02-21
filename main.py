fd0b0d0f6212a206758af6131f62d58c630859c5
Annamalaii45 <100117262+Annamalaii45@users.noreply.github.com>
2022-02-21 14:21:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+Shop_name= """                          Amazon All In One Centre"""
+
+customer="Customer name:"+"Annamalaii"
+age="Age:"+"17"
+address="Address:"+"35/a Gandhi st kilpennathur,Tvm,604601"
+
+product="Product:"+"Sunglass"
+brand="Brand:"+"Montblanc"
+Cost_Of_The_Product=700
+Gst=102.14
+Cgst=49.9
+
+cost_of_the_product="Cost Of The Product="+"700"
+gst="Gst="+"102.14"
+cgst="Cgst="+"49.9"
+
+Final = "Total"
+Total=Cost_Of_The_Product + Gst +Cgst
+
+Product_Description=""" This is a good product which I have ever seen before through
+ My Eyes,nose,ears,legs,hands,Etc....."""
+
+Ending="""                         Thankyou,Please Visit Us Again"""
+
+print(Shop_name)
+print(customer)
+print(age)
+print(address)
+print()
+print(product)
+print(brand)
+print()
+print(cost_of_the_product)
+print(gst)
+print(cgst)
+print()
+print(Final)
+print(Total)
+print()
+print(Product_Description)
+print()
+print(Ending)
